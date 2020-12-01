a767a8c46293f2a2c40365243bf20e09948123e1
Damian Bocanegra <d-b172@users.noreply.github.com>
2020-11-30 19:21:33 -0600

    Added Navmesh agent and built terrian

---
+++ RPG/Assets/Scripts/Mover.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    private NavMeshAgent agent;
+    private Ray lastRay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+
+        if(hasHit)
+        {
+            agent.destination = hit.point;
+        }
+    }
+}
